760543b84ab66360da6b93f9b57d509ddc40ad48
Danter28 <38162330+Danter28@users.noreply.github.com>
2019-10-30 16:14:40 -0600

    Add files via upload

---
+++ Persona.h
@@ -0,0 +1,29 @@
+#ifndef _PERSONA_
+#define _PERSONA_
+#include<iostream>
+#include<string>
+using std::string;
+class Persona {
+
+private:
+	string cedula;
+	string nombreCompleto;
+	string email;
+	string telefono;
+public:
+	Persona();
+	Persona(string,string,string,string);
+	~Persona();
+	string getCedula();
+	string getNombreCompleto();
+	string getEmail();
+	string getTelefono();
+	void setCedula(string);
+	void setNombreCompleto(string);
+	void setEmail(string);
+	void setTelefono(string) ;
+	string toString();
+
+
+};
+#endif // !_PERSONA_
